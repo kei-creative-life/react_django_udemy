d4dcddb653d1dd596832404fd0cb73ab97eb18e2
KEI <68884990+kei1992@users.noreply.github.com>
2021-02-07 22:55:21 +0900

    create-usestate-array

---
+++ src/components/Basic2.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Basic2 = () => {
+  const [products, setProducts] = useState([]);
+  const newProducts = () => {
+    setProducts([...products, { id: products.length, name: "Hello React" }]);
+  };
+  console.log(products);
+  return (
+    <>
+      <button onClick={newProducts}>Add New Product</button>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              {product.name} id: {product.id}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Basic2;
