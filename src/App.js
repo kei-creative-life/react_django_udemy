a7a7a2e0a90f8976ac8effc1ae16a93de93d81d3
KEI <68884990+kei1992@users.noreply.github.com>
2021-02-08 00:26:15 +0900

    use-effect-cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Basic2 from "./components/Basic2";
+// import Basic2 from "./components/Basic2";
+// import BasicUseEffect from "./components/BasicUseEffect";
+import TimerContainer from "./components/TimerContainer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic2 />
+        {/* <Basic2 /> */}
+        {/* <BasicUseEffect /> */}
+        <TimerContainer />
       </header>
     </div>
   );
