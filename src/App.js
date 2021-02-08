89214343b8a9d9932c5fca9998e2bcb9d454a56f
KEI <68884990+kei1992@users.noreply.github.com>
2021-02-08 16:59:45 +0900

    usecontext-usereducer-mixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ApiFetch from "./components/ApiFetch";
@@ -8,10 +8,28 @@ import ApiFetch from "./components/ApiFetch";
 import AppContext from "./contexts/AppContext";
 import B from "./components/B";
 import BasicReducer from "./components/BasicReducer";
+import CompB from "./components/CompB";
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "add_1":
+      return currentState + 1;
+    case "multiple_3":
+      return currentState * 3;
+    case "reset":
+      return initialState;
+    default:
+      return currentState;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <AppContext.Provider value={"value from App.js"}>
+    <AppContext.Provider
+      value={{ countProvider: count, dispatchProvided: dispatch }}
+    >
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +38,9 @@ function App() {
           {/* <TimerContainer /> */}
           {/* <ApiFetch /> */}
           {/* <B /> */}
-          <BasicReducer />
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
         </header>
       </div>
     </AppContext.Provider>
